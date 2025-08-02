9c8f74e96a045c26d6ddf0243f48dc3cf060126f
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-08-02 09:48:36 +0500

    add

--- app/_document.tsx
+++ app/_document.tsx
@@ -4,7 +4,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script src="https://telegram.org/js/telegram-web-app.js?59"></script>
+        <script src="https://telegram.org/js/telegram-web-app.js?59">
+        </script>
+        <script
+          async
+          src="https://telegram.org/js/telegram-widget.js?22"
+          data-telegram-login="Findecor"
+          data-size="medium"
+          data-onauth="onTelegramAuth(user)"
+          data-request-access="write"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function onTelegramAuth(user) {
+                alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+              }
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />       {/* Bu yerga barcha sahifa content kelyapti */}
