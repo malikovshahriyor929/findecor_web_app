c56a5c46cec54d945fb44b36a58d9dd080d3fc00
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-08-06 16:20:54 +0500

    add loading

--- components/auth/login/index.tsx
+++ components/auth/login/index.tsx
@@ -5,6 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import toast from "react-hot-toast";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { Loader2 } from "lucide-react";
 declare global {
   interface Window {
     Telegram: {
@@ -54,7 +55,7 @@ const LoginComponents = () => {
     const tg = window.Telegram.WebApp;
     tg.ready();
     const actualInitDataFromTelegram =
-     tg.initData;
+      tg.initData;
     // "user=%7B%22id%22%3A1910930481%2C%22first_name%22%3A%22Shahriyor%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22Malikov_Shahriyor%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2Fu1yyIL-OgzVaLepKDPzqLB1QEUI8weRpEP3pkHze5f0.svg%22%7D&chat_instance=5994368464737266795&chat_type=private&auth_date=1754465671&signature=rtDnqd1p-o9Kfzct1cFVk3FAoItdxRNOj6igXcjgEEIL10O0PQS8QY1gl2-H02IEWjL3jNp_E3iqGEBCq8N3Aw&hash=1efe547f8e96c83aa688e9f1cd68a0d0de8947c15c70bed12a9e25e08f3adb61"
     if (!actualInitDataFromTelegram) {
       toast.error("Telegram initData bo‘sh, qayta yuklanmoqda...");
@@ -83,12 +84,9 @@ const LoginComponents = () => {
   return (
     <div className="flex items-center justify-center h-screen flex-col gap-3">
 
-      <button
-        className="bg-red-500 text-white px-4 py-2 rounded"
-        onClick={ () => window.location.reload() }
-      >
-        reload
-      </button>
+      <div className="absolute top-0 left-0 size-full flex items-center justify-center  ">
+        <Loader2 className=" animate-spin " size={ 30 } />
+      </div>
     </div>
   );
 };
